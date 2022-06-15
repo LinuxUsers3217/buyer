39eff409235d0bc51228a7541b42d305793c9a51
T-Rex21996.1 <88968137+TRex219961@users.noreply.github.com>
2022-06-15 10:19:47 +0700

    Create DownloadWin81.sh

---
+++ DownloadWin81.sh
@@ -0,0 +1,11 @@
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+rm -rf ngrok-stable-linux-amd64.zip
+./ngrok authtoken 268tKkixRMBBxHKeBCpS6s72ohm_7UvxkT4VkysGcyKi2JpZN
+nohup ./ngrok tcp --region ap 5900 &>/dev/null &
+sudo apt-get update && apt-get install qemu -y
+sudo apt install qemu-utils -y
+sudo apt install qemu-system-x86-xen -y
+sudo apt install qemu-system-x86 -y
+wget -O windows10.img 'https://www.dropbox.com/s/abhgiyi2skwioc1/a.qcow2?dl=0'
+wget -O idk.iso 'https://drive.google.com/uc?export=download&id=1wDL8vo9mmYKw1HKXZzaYHoKmzSt_wXai'
